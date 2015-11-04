168165df3eeeacee16d09a2395a31cc31e9c3a11
RemyTartiere <remy@MacBook-Pro-de-Matthieu.local>
2015-11-04 13:02:01 +0100

    servlet accessory Json
    create servlet accessory, send the JSON

---
+++ src/services/Accessories.java
@@ -0,0 +1,75 @@
+package services;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import bl.facade.EventFacade;
+import persistance.MySQLDatabase;
+
+/**
+ * Servlet implementation class Accessories
+ */
+@WebServlet(name = "accessories", urlPatterns = { "/accessories" })
+public class Accessories extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Accessories() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		
+		if(!MySQLDatabase.getInstance().open())
+		{
+			out.print("Cannot instantiate MySQL Connexion");
+		}
+		else
+		{
+			Gson gson = new Gson();
+			String json = gson.toJson(new EventFacade().getAllAccessories().getList());
+			System.out.println(json);
+			out.print(json);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}

77af691651145238e75387e613a8e419d01ab412
RemyTartiere <remy@MacBook-Pro-de-Matthieu.local>
2015-11-09 13:45:48 +0100

    login degeux

---
+++ src/services/login.java
@@ -0,0 +1,55 @@
+package services;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bl.facade.UserFacade;
+import utilitaries.Rest;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		HashMap<String,String> map = Rest.GetParameters(request);
+		
+		if(map.get("login").equals("toto") && map.get("mdp").equals("toto")){
+		
+		Rest.AddObjectAsJSONResponse(UserFacade.instance().getUser(7,""),response);
+		}
+		else{
+			Rest.AddObjectAsJSONResponse(null, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

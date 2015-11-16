19741d4078a3247b579923502eba36aa42600893
Joris Maillet <jorismaillet@gmail.com>
2015-11-16 11:45:26 +0100

    Get all events

--- src/services/Events.java
+++ src/services/Events.java
@@ -1,55 +1,46 @@
 package services;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.HashMap;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import bl.facade.EventFacade;
-import dal.product.generic.Event;
+import utilitaries.Rest;
 
-@Path("events")
-@Produces("application/json")
-@Consumes("application/json")
-public class Events {
-	
-    @Context
-    private UriInfo context;
-
+/**
+ * Servlet implementation class events
+ */
+@WebServlet("/events")
+public class events extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
-    public Events() {
+    public events() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * Retrieves representation of an instance of events
-     * @return an instance of a List of event
-     */
-    @GET
-    public List<Event> getEvents() {
-    	
-    	return EventFacade.instance().getAllEvents();
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//HashMap<String,String> map = Rest.GetParameters(request);
+		Rest.AddObjectAsJSONResponse(EventFacade.instance().getAllEvents(),response);
+}
 
-	@GET
-	@Path("{id}")
-	public Event getEvent(@PathParam("id") String id) {
-		return EventFacade.instance().getAllEvents().get(0);
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
-	
-    /**
-     * PUT method for updating or creating an instance of Test3
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
-    @PUT
-    public void putJson(String content) {
-    }
 
 }
